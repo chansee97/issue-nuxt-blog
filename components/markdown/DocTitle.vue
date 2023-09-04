c50682fba37e06233435e54e2b297e5deee711e2
Coffee-crocodile <1147347984@qq.com>
2023-09-04 16:44:59 +0800

    fix: optimize md style

--- components/markdown/DocTitle.vue
+++ components/markdown/DocTitle.vue
@@ -7,24 +7,24 @@ const { issue } = defineProps(['issue'])
 <template>
   <div>
     <h1>{{ issue.title }}  </h1>
-    <div class="op-50 mb-4em">
+
+    <div class="op-70 mb-4em">
+      <!-- Time -->
       <div>
-        <!-- Time -->
         {{ formattedDate(issue.created_at) }}
+        <a :href="issue.html_url" target="_blank">Issue </a>
+        <div class="i-icon-park-outline-arrow-right-up" />
+      </div>
 
-        <!-- Tags -->
-        <div class="flex gap-2">
-          <NuxtLink
-            v-for="label in issue.labels" :key="label.id" :to="`/tags/${label.name}`"
-          >
-            #{{ label.name }}
-          </NuxtLink>
-        </div>
-
-        <!-- GO issue -->
-        <div>
-          <div class="i-simple-icons-github" />
-          <a :href="issue.html_url" target="_blank"> Go issue </a>
+      <!-- Tags -->
+      <div class="flex gap-2 m-y-0.5em">
+        <div
+          v-for="label in issue.labels" :key="label.id"
+          :style="{ backgroundColor: `#${label.color}77` }"
+          class="px-2 rd-1 cursor-pointer"
+          @click="$router.push(`/tags/${label.name}`)"
+        >
+          # {{ label.name }}
         </div>
       </div>
     </div>
