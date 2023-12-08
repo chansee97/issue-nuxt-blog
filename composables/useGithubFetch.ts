e23036887a2c469d9f722f4c6c55e5ec0df72f0d
chen.home <chen.dev@foxmail.com>
2023-12-08 13:24:42 +0800

    fix: move local env var

--- composables/useGithubFetch.ts
+++ composables/useGithubFetch.ts
@@ -1,16 +1,10 @@
 const { VITE_GITHUB_TOKEN } = import.meta.env
 
 export async function useGithubFetch<T>(url: string) {
-  let token = ''
-
-  // 修正token
-  if (VITE_GITHUB_TOKEN)
-    token = VITE_GITHUB_TOKEN.replace('.', '')
-
   const { data, error } = await useFetch<T>(url, {
     onRequest({ options }) {
       options.headers = {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${VITE_GITHUB_TOKEN}`,
       }
     },
   })
